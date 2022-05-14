b8c853b16d366ea29066d18d18c4337b60219b15
srgrusso <srgrusso240@gmail.com>
2022-05-14 12:40:48 -0500

    sm8150-common: Drop DeviceSettings

--- common.mk
+++ common.mk
@@ -81,10 +81,6 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# DerpFest Device Settings
-PRODUCT_PACKAGES += \
-    DeviceSettings
-
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
