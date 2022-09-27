7468708ba25e0d9a3797c8490aa53cf94d4f1248
Josh Wu <joshwu@google.com>
2022-09-26 19:13:34 -0500

    sm8150-common: Switch BtAudio to AIDL

--- common.mk
+++ common.mk
@@ -97,7 +97,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.soundtrigger@2.1-impl:32 \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio-impl \
     audio.primary.default \
     audio.primary.msmnile \
     audio.bluetooth.default \
