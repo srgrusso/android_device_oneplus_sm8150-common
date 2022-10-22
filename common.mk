ae15069ace539e19dbc83c05932617c99f6b061b
AnierinB <anierin@evolution-x.org>
2022-10-22 01:05:21 +0000

    sm8150-common: Configure & ship OPlusExtras
    - Device specific features are set in their respective trees.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Change-Id: I5344ac177e49e850fa772f818535b144c9db6308

--- common.mk
+++ common.mk
@@ -21,9 +21,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     AntHalService-Soong
 
-# Alert slider
+# OPlusExtras
 PRODUCT_PACKAGES += \
-    KeyHandler \
+    OPlusExtras \
     tri-state-key-calibrate
 
 # Audio
@@ -179,6 +179,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.oplus.rc \
+    init.oplus_extras.rc \
     init.oplus.sh \
     init.qcom.early_boot.sh \
     init.qcom.rc \
@@ -238,7 +239,8 @@ PRODUCT_PACKAGES += \
     FrameworksResTarget \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
-    OPlusSystemUIResCommon
+    OPlusSystemUIResCommon \
+    OPlusExtrasResCommon
 
 # Partitions
 PRODUCT_PACKAGES += \
