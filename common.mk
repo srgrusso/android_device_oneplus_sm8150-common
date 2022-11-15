78a94cf41177d4ebbdf21bab77bdb119a81c291f
ShevT <evgeny.shishkov@gmail.com>
2022-11-15 11:36:19 +0300

    sm8150-common: Implement OnePlus Dolby support

--- common.mk
+++ common.mk
@@ -247,6 +247,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml
 
 PRODUCT_COPY_FILES += \
@@ -304,6 +305,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OPlusExtras \
     tri-state-key-calibrate
+
+# OnePlus Dolby
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/dax-default.xml
+
+TARGET_EXCLUDES_AUDIOFX := true
 
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
