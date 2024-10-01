4f6a1022e4f5dde625e0b7bb083081f0f89ddc5c
Bruno Martins <bgcngm@gmail.com>
2024-10-01 22:35:45 +0100

    sm8150-common: Address seccomp filter failures
    Fixes:
     init: ... started service 'vendor.atfwd' has pid 12901
     init: Service 'vendor.atfwd' (pid 12901) received signal 31
     init: Sending signal 9 to service 'vendor.atfwd' (pid 12901) process group...
    
    Change-Id: Ic259464520db6e77f06a3d0820a63662aa91659c

--- extract-files.sh
+++ extract-files.sh
@@ -116,6 +116,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i "s/\/vendor\/lib\/liba2dpoffload.so/\/odm\/lib\/liba2dpoffload.so\x00\x00\x00/" "${2}"
             ;;
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+            [ "$2" = "" ] && return 0
+            grep -q "gettid: 1" "${2}" || echo -e "\ngettid: 1" >> "${2}"
+            ;;
         vendor/lib64/hw/com.qti.chi.override.so)
             [ "$2" = "" ] && return 0
             grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
