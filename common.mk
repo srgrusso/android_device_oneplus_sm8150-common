5f40a40ed8a430e1e9462915567d152874b011f8
Pranav Vashi <neobuddy89@gmail.com>
2025-02-09 22:35:54 +0530

    sm8150-common: Add hide cutout emulations
    Change-Id: I1ea1a31c1e4c968ae7b884236e8361288ea1e258

--- common.mk
+++ common.mk
@@ -128,6 +128,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
 
+# Set support hide display cutout feature
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_hide_display_cutout=true
+
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay \
+    AvoidAppsInCutoutOverlay
+
 # Dolby
 PRODUCT_PACKAGES += \
     XiaomiDolby
